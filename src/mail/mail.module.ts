8b8e6b39c0352df39e7177eca7f97bb0598ecb89
shafinsan <eliasjabershafin599@gmail.com>
2025-09-14 21:48:21 +0600

    Fix role seeding and improve auth service

--- src/mail/mail.module.ts
+++ src/mail/mail.module.ts
@@ -1,24 +1,26 @@
 import { Module } from '@nestjs/common';
 import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { join } from 'path';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          user: 'eliasjabershafin100@gmail.com',
-          pass: 'qwbdiiwlvmmopcuy',
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        transport: {
+          host: configService.get('SMTP_HOST'),
+          port: configService.get('SMTP_PORT'),
+          secure: configService.get('SMTP_SECURE') === 'true',
+          auth: {
+            user: configService.get('SMTP_USER'),
+            pass: configService.get('SMTP_PASS'),
+          },
         },
-      },
-      defaults: {
-        from: '"No Reply" <eliasjabershafin100@gmail.com>',
-      },
+        defaults: {
+          from: `"No Reply" <${configService.get('SMTP_USER')}>`,
+        },
+      }),
+      inject: [ConfigService],
     }),
   ],
   exports: [MailerModule],
